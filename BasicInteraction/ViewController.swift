be66c9dbac2cf08df0725ed90a9fe6085574aa3d
Luiz Fernando Santiago <luizfso@gmail.com>
2015-06-19 03:11:19 -0300

    test

--- BasicInteraction/ViewController.swift
+++ BasicInteraction/ViewController.swift
@@ -8,8 +8,29 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var simpleTextField: UITextField!
+    
+    @IBOutlet weak var simpleLabel: UILabel!
+    
+    @IBAction func changeLabel(sender: AnyObject) {
+        simpleLabel.text = "Hello, " + simpleTextField.text + "!"
+        self.simpleTextField.resignFirstResponder()
+        
+        
+    }
+    
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return false
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.view.endEditing(true)
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
